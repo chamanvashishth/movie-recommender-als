4b4eeb0ec1187698fee5baaae3a105ef44eaa2cd
Chaman Vashishth <chamanvashishth133@gmail.com>
2026-06-08 07:17:40 +0530

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,13 @@
+"""
+MovieLens ALS Recommender System
+
+Modules:
+--------
+data_loader          : Dataset download and preprocessing
+bias_model           : Global/User/Item bias computation
+als_optimizer        : ALS optimization routines
+matrix_factorization : Main recommendation model
+cold_start           : Genre-based recommendations
+metrics              : Evaluation metrics
+explainability       : Recommendation explanations
+"""
